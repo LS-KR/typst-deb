aae324771519af0de4d581a7f76ec31344086832
Elihuso Quigley <Elihuso@outlook.com>
2025-05-05 12:38:10 +0800

    [+] build sh

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+curl -s https://api.github.com/repos/typst/typst/releases/latest \
+    | grep "https://.*x86_64.*linux.*.tar.xz" \
+    | cut -d : -f 2,3 \
+    | tr -d \" \
+    | wget -qi -
+
+fileName=$(curl -s https://api.github.com/repos/typst/typst/releases/latest | grep ".*x86_64.*linux.*.tar.xz\"," | cut -d : -f 2,3 | tr -d \" | tr -d ,)
+printf "found release file: %s\n" $fileName
+
+tar --xz -xvf $fileName
+bash ./clean.sh
+
+name=${fileName%.tar.xz}
+name=$(echo $name | tr -d " ")
+echo $name
+
+mkdir typst
+mkdir -p typst/usr/bin
+mkdir -p typst/usr/share/doc/typst
+mkdir -p typst/DEBIAN
+
+cp -f "./${name}/typst" typst/usr/bin/typst
+cp -f "./${name}/LICENSE" "./${name}/NOTICE" "./${name}/README.md" "typst/usr/share/doc/typst"
+cp -f "DEBIAN/control" "typst/DEBIAN/control"
+
+sudo dpkg -b ./typst
+
+bash ./clean.sh
---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf ./*.xz*
+rm -rf typst/
+rm -rf typst-x86_64*/
